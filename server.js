41283eaaa09c528ab775d378f537fa902b0c09ec
GabrielGRCosta <galvaogabriel890@gmail.com>
2022-07-10 18:20:04 -0300

    ejs

--- server.js
+++ server.js
@@ -3,30 +3,41 @@ var path = require("path");
 var app = express();
 
 
+
 app.get("/",function(req,res){
-    res.render("login");
+    res.render("login.ejs");
+});
+
+app.get("/",function(req,res){
+  res.render("login.ejs");
 });
 
 app.get("/index2.html",function(req,res){
-  res.render("home");
+  res.render("home.ejs");
 });
 
 app.get("/index3.html",function(req,res){
-  res.render("catalogo");
+  res.render("catalogo.ejs");
 });
 
 app.get("/index4.html",function(req,res){
-  res.render("jogo");
+  res.render("jogo.ejs");
 });
 
 app.get("/cadastro.html",function(req,res){
-  res.render("cadastro");
+  res.render("cadastro.ejs");
 });
+
+app.use('/static', express.static('images'));
 
 app.engine("html", require("ejs").renderFile);
 app.set("port", process.env.PORT || 8000);
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "html");
+app.set("view engine", "ejs");
+
+app.get('/todos/:id', function (req, res) {
+  console.log('Request Id:', req.params.id);
+});
 
 
 
